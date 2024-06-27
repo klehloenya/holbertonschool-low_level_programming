1cf47b91f91b1f3af2f260e55a1d95db748c64fb
klehloenya <keketsolehloenya516@gmail.com>
2024-06-27 15:10:06 +0000

    0-isupper.c

---
+++ more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+_putchar.c
+
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: On success
+ * On error, -1 is returned, abd errno is set appropriately.
+ */
+int _putchar(char)
+	return (write(1, $c, 1));
