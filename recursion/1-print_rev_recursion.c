62bfc3b20b4e9b09fd709e6240a4413046c88125
klehloenya <keketsolehloenya516@gmail.com>
2024-07-10 10:09:55 +0000

    holbertonschool-low_level_programming

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,11 +7,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0') 
-    {
-        return;
-    }
-    _print_rev_recursion(s + 1);
-    putchar(*s);
+    if (*s == '\0')
+	    return;
+    s++;
+	    _print_rev_recursion(s);
+	    s--;
 }
 
