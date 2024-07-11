0e83dfb73820a813d1bd63988cb45402cb331527
klehloenya <keketsolehloenya516@gmail.com>
2024-07-11 13:05:53 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,49 +6,54 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: pointer to concat to string
+ * Return: pointer to concatenated string
  * which memory was allocated for
  */
 char *str_concat(char *s1, char *s2)
 {
-	size_t len1, len2, i, j;
-	char *ptr;
+    size_t len1, len2, i, j;
+    char *ptr;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
+    if (s1 == NULL)
+    {
+        s1 = "";
+    }
 
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	len1 = 0;
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
-	len2 = 0;
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+    if (s2 == NULL)
+    {
+        s2 = "";
+    }
 
-	if (ptr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	for (i = 0; i < len1; i++)
-	{
-		ptr[i] = s[i];
-	
-	}
-	for (j = 0; j <= len2; j++)
-	{
-		ptr[i] = s[j];
-		i++;
-	}
-	return (ptr);
+    len1 = 0;
+    while (s1[len1] != '\0')
+    {
+        len1++;
+    }
+
+    len2 = 0;
+    while (s2[len2] != '\0')
+    {
+        len2++;
+    }
+
+    ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+
+    if (ptr == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < len1; i++)
+    {
+        ptr[i] = s1[i];
+    }
+
+    for (j = 0; j < len2; j++)
+    {
+        ptr[i + j] = s2[j];
+    }
+
+    ptr[i + j] = '\0';
+
+    return (ptr);
 }
