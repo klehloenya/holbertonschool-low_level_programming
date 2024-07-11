a9402e2ac58b6618d81f00f7b56a9e97b1e0826d
klehloenya <keketsolehloenya516@gmail.com>
2024-07-11 13:15:06 +0000

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -10,36 +10,23 @@
  */
 char *_strdup(char *str)
 {
-	size_t i, j;
-	char *strdout;
+    char *dup_str;
+    size_t len, i;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	
-	}
+    if (str == NULL)
+        return (NULL);
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	
-	}
+    len = 0;
+    while (str[len] != '\0')
+        len++;
 
-	strdout = malloc(sizeof(char) * (i + 1));
+    dup_str = malloc(sizeof(char) * (len + 1));
+    if (dup_str == NULL)
+        return (NULL);
 
-	if (strdout == NUL)
-	{
-		return (NULL);
-	
-	}
+    for (i = 0; i < len; i++)
+        dup_str[i] = str[i];
+    dup_str[len] = '\0';
 
-	for (j = 0; j <= i; j++)
-	{
-		strdout[j] = str[j];
-	 
-	}
-	
-		return (strdout)
-
+    return (dup_str);
 }
