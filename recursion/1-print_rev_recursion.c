fa8559f2998d6522698b4af9fca96ca12bea01bf
klehloenya <keketsolehloenya516@gmail.com>
2024-07-15 17:01:17 +0000

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,10 +7,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-	    return;
-    s++;
-	    _print_rev_recursion(s);
-	    s--;
+    if (*s)
+    {
+	    _print_rev_recursion(s + 1);
+	    _putchar(*s);
+    }
 }
 
